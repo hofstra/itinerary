863b2fc8bd9659c5914139eb0d66267212bdf310
Mark Reeves <mjr@clearbold.com>
2015-03-04 14:28:57 -0500

    Tweaked the buried: # in the popup to fall back to 0.

--- _source/ui/js/script.js
+++ _source/ui/js/script.js
@@ -34,18 +34,3 @@ map.setView([51.514, -0.0881481171],14);
 // TODO: Code for single location markers
 // var testMarker = L.marker([51.5133631072273, -0.0889500975608826]).addTo(map);
 // testMarker.bindPopup("This is <b>formatted</b> content about this location.").openPopup();
-/*
-testParish.bindPopup('<div style="max-height: 175px; overflow-y: scroll;">'
-        + '<h3>St. Andrew\'s-Holborn</h3>'
-        + '<dl>'
-            + '<dt><a href="#event-6">December 20-27</a></dt>'
-            + '<dd><b>Buried: 0.</b> Both St. Brides and St. James Clarkenwell are adjacent to St. Andrews Holborn. H.F. notes that the usual number for burials per week in St. Giles and St. Andrews was roughly 12 to 17 or 19 (3). He also notes the usual number of burials per week in St. Brides and St. James were roughly 4 to 6 or 8 (5).</dd>'
-            + '<dt>December 27-January 3</dt>'
-            + '<dd><b>Buried: 6.</b></dd>'
-            + '<dt>January 3-10</dt>'
-            + '<dd><b>Buried: 11</b></dd>'
-            + '<dt>January 10-17</dt>'
-            + '<dd><b>Buried: 12</b></dd>'
-        + '</dl>'
-    + '</div>');//.openPopup();
-*/
