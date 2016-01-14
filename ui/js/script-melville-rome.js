0b100c1bd234bacf3b864a9081c5888a652f25f9
Mark Reeves <mjr@clearbold.com>
2016-01-14 15:47:18 -0500

    Successfully rolled content up into popups for each waypoint. Set up events/handlers to clear bound popup content when waypoints are clicked again.

--- ui/js/script-melville-rome.js
+++ ui/js/script-melville-rome.js
@@ -96,6 +96,7 @@ $(document).ready(function() {
                     {
                         'id' : $waypointId,
                         'marker' : $marker,
+                        'waypoint' : $event.waypoint,
                         'latitude' : $event.latitude,
                         'longitude' : $event.longitude
                     }
@@ -103,7 +104,7 @@ $(document).ready(function() {
                 $waypointId++;
 
                 // Fetch and fill in template here
-                $itineraryEventContent = $event.waypoint;
+                $itineraryEventContent = '<h3>' + $event.waypoint + '</h3>';
                 console.log($itineraryEventContent);
                 // TODO: Roll up locations and observations for each event into the popup, using template approach
                 // ...But that should really happen in the Jekyll-generated JSON file, so that we have a consistent
@@ -117,21 +118,55 @@ $(document).ready(function() {
         //console.log( "Request Failed: " + err );
     })
 
-    // When a parish in the data table is clicked
+    $.each( $waypointMarkers, function( $i, marker) {
+        marker.marker.on('click', function(e) {
+            $(this).bindPopup('<h3>' + marker.waypoint + '</h3>');
+            $(this).openPopup;
+        })
+    });
+
     $(".waypoint a").on('click', function(e) {
         //e.preventDefault(); // Let the # link scroll us to the top
+        var $waypoint = $(this).data('waypoint-id');
+        var $waypointName = $(this).html();
         var $waypointId = $(this).data('waypoint-id').split('-')[1];
         var $itinerarySequence = $(this).data('waypoint-id');
 
         //currentEvent($itinerarySequence, $parishPolygons, $itineraryData, $locationMarkers);
+        // Per Itinerary I, this can go in a function
+        $.each( $paths, function( $i, path) {
+            path.path.closePopup();
+        })
         $.each( $waypointMarkers, function( $i, marker) {
             marker.marker.closePopup();
+            marker.marker.bindPopup(marker.waypoint);
         });
         $.each( $waypointMarkers, function( $i, marker ) {
-            console.log($i);
-            console.log($waypointId);
             if ($i+1 == $waypointId)
+            {
+                // This needs to go in a template
+                $popup = '<div style="max-height: 175px; overflow-y: scroll;">';
+                console.log($waypointName);
+                $popup += '<h3>' + $waypointName + '</h3>';
+                $.each( $('tr.'+$waypoint), function( $i, tr ) {
+                    console.log(tr);
+                    ($(tr).children('td.date').html().length == 0) ? console.log('Empty') : console.log($(tr).children('td.date').html());
+                    if ( $(tr).children('td.date').html().length > 0 )
+                        $popup += '<p><b>' + $(tr).children('td.date').html() + '</b></p>';
+                    ($(tr).children('td.location').html().length == 0) ? console.log('Empty') : console.log($(tr).children('td.location').html());
+                    if ( $(tr).children('td.location').html().length > 0 )
+                        $popup += '<p>' + $(tr).children('td.location').html() + '</p>';
+                    ($(tr).children('td.observation').html().length == 0) ? console.log('Empty') : console.log($(tr).children('td.observation').html());
+                    if ( $(tr).children('td.observation').html().length > 0 )
+                        $popup += '<p><i>' + $(tr).children('td.observation').html() + '</i></p>';
+                    ($(tr).children('td.text').html().length == 0) ? console.log('Empty') : console.log($(tr).children('td.text').html());
+                    if ( $(tr).children('td.text').html().length > 0 )
+                        $popup += '<p><i>' + $(tr).children('td.text').html() + '</i></p>';
+                } )
+                $popup += '</div>';
+                marker.marker.bindPopup($popup);
                 marker.marker.openPopup();
+            }
         })
     })
 })
